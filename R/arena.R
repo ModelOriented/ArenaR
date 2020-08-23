c636c2fc7bf7bf500782cb6aa852d5bafccc8251
Piotr Piątyszek <piotrp@wektor.xyz>
2020-08-23 20:28:46 +0200

    Update print functions

--- R/arena.R
+++ R/arena.R
@@ -125,68 +125,6 @@ create_arena <- function(live = FALSE,
   )
 }
 
-#' Prints static arena summary
-#'
-#' @param x \code{arena_static} object
-#' @param ... other parameters
-#' @export
-#' @return None
-#' @examples
-#' library("DALEX")
-#' library("arenar")
-#' library("dplyr", quietly=TRUE, warn.conflicts = FALSE)
-#' # create a model
-#' model <- glm(m2.price ~ ., data=apartments)
-#' # create a DALEX explainer
-#' explainer <- DALEX::explain(model, data=apartments, y=apartments$m2.price)
-#' # prepare observations to be explained
-#' observations <- apartments[1:3, ]
-#' # rownames are used as labels for each observation
-#' rownames(observations) <- paste0(observations$construction.year, "-", observations$surface, "m2")
-#' # generate static arena for one model and 3 observations
-#' arena <- create_arena(live=FALSE) %>% push_model(explainer) %>% push_observations(observations)
-#' # print summary
-#' print(arena)
-print.arena_static <- function(x, ...) {
-  cat("===== Static Arena Summary =====\n")
-  jstr <- get_json_structure(x)
-  cat(paste("Models:", paste(jstr$models, collapse = ", "), "\n"))
-  cat(paste("Observations:", paste(jstr$observations, collapse = ", "), "\n"))
-  cat(paste("Variables:", paste(jstr$variables, collapse = ", "), "\n"))
-  cat(paste("Plots count:", length(jstr$data), "\n"))
-}
-
-#' Prints live arena summary
-#'
-#' @param x \code{arena_live} object
-#' @param ... other parameters
-#' @export
-#' @return None
-#' @examples
-#' library("DALEX")
-#' library("arenar")
-#' library("dplyr", quietly=TRUE, warn.conflicts = FALSE)
-#' # create a model
-#' model <- glm(m2.price ~ ., data=apartments)
-#' # create a DALEX explainer
-#' explainer <- DALEX::explain(model, data=apartments, y=apartments$m2.price)
-#' # prepare observations to be explained
-#' observations <- apartments[1:30, ]
-#' # rownames are used as labels for each observation
-#' rownames(observations) <- paste0(observations$construction.year, "-", observations$surface, "m2")
-#' # generate live arena for one model and 30 observations
-#' arena <- create_arena(live=TRUE) %>% push_model(explainer) %>% push_observations(observations)
-#' # print summary
-#' print(arena)
-print.arena_live <- function(x, ...) {
-  cat("===== Live Arena Summary =====\n")
-  jstr <- get_json_structure(x)
-  cat(paste("Models:", paste(jstr$models, collapse = ", "), "\n"))
-  cat(paste("Observations:", paste(jstr$observations, collapse = ", "), "\n"))
-  cat(paste("Variables:", paste(jstr$variables, collapse = ", "), "\n"))
-  cat("Remember to start server with arena_run(arena)\n")
-}
-
 #' Adds model to arena
 #'
 #' If arena is static it will start calculations for all already pushed
