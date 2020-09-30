69cc3c31228135a5797d37ddde1c79682cb8f89a
Piotr Piątyszek <piotrp@wektor.xyz>
2020-10-01 00:27:03 +0200

    Add URLdecode of param_label in /attribute handler

--- R/live.R
+++ R/live.R
@@ -30,7 +30,7 @@ run_server <- function(arena, port = 8181, host = "127.0.0.1",
   if (is.null(arena) || !is(arena, "arena_live")) {
     stop("Invalid arena argument")
   }
-  pr <- plumber::plumber$new()
+  pr <- plumber::Plumber$new()
   json_structure <- get_json_structure(arena)
 
   # helper function to find explainer for given name
@@ -202,11 +202,14 @@ run_server <- function(arena, port = 8181, host = "127.0.0.1",
   pr$handle("GET", "/attribute/<param_type>/<param_label>",
             function(req, res, param_type = "", param_label = "") {
     if (!arena$params$enable_attributes) return(res$status <- 404)
+    # replace %20 with space etc.
+    param_label <- URLdecode(param_label)
     if (param_type == "model") {
       explainer <- get_explainer(param_label)
       if (is.null(explainer)) return(res$status <- 404)
       get_model_attributes(arena, explainer)
     } else if (param_type == "observation") {
+      print(param_label)
       observation <- get_observation(param_label)
       if (is.null(observation)) return(res$status <- 404)
       get_observation_attributes(arena, observation)
